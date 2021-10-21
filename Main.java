1fcec6b90f7b163b516119cfe1934cf4dbe641f9
PRunich <p.runich@oblakogroup.ru>
2021-10-21 11:49:57 +0300

    array list

--- Main.java
+++ Main.java
@@ -1,65 +1,91 @@
 package com.company;
-import java.util.Scanner;
-import java.util.regex.Pattern;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
-        Auth auth = new Auth();
-        auth.fillLogin();
+        HubList<String> youList = new HubList<String>();
+        youList.add("ccc");
+        youList.add("b");
+        youList.add("aa");
+        System.out.println(youList); //[ccc, b, aa]
+        System.out.println(youList.find("b")); //1
+        System.out.println(youList.find("s")); //-1
+        youList.sort(new HubStringComparator());
+        System.out.println(youList); //[b, aa, ccc]
+        System.out.println(youList.removeAt(1)); //aa
+        System.out.println(youList); //[b, ccc]
     }
 }
 
-class Auth {
-    private String login;
+class HubStringComparator implements Comparator {
+    @Override
+    public int compare(Object str1, Object str2) {
+        return Integer.compare(((String) str1).length(), ((String) str2).length());
+    }
+}
 
-    public void fillLogin() {
-        Scanner scanner = new Scanner(System.in);
+class HubList<E> {
+    private int capacity = 0;
+    private int size = 0;
+    E[] array;
 
-        do {
-            System.out.print("Введите email или телефон: ");
-            login = scanner.nextLine();
-        } while(!isLoginValid());
+    public HubList() {
+        capacity = 1;
+        array = (E[]) new Object[capacity];
+    }
 
-        Coder backend = new Coder();
-        backend.sendCode();
+    public void add(E element) {
+        if (size + 1 == capacity) increaseCapacity();
 
-        System.out.print("Введите code: ");
-        int code = scanner.nextInt();
+        array[size++] = element;
+    }
 
-        if (backend.checkCode(code)) {
-            System.out.println("Удачно");
-        } else {
-            System.out.println("Неверный");
+    private void increaseCapacity() {
+        capacity = capacity * 2;
+        E[] newArray = (E[]) new Object[capacity];
+
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[i];
+            array[i] = null;
+        }
+
+        array = newArray;
+    }
+
+    public E removeAt(int index) {
+        E object = array[index];
+        array[index] = null;
+
+        System.arraycopy(array, index + 1, array, index, size - index);
+
+        size--;
+        return object;
+    }
+
+    public void sort(Comparator<? super E> c) {
+        for(int i = 0 ; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (0 < c.compare(array[i], array[j])) {
+                    E oldEl = array[i];
+
+                    array[i] = array[j];
+                    array[j] = oldEl;
+                }
+            }
         }
     }
 
-    private boolean isLoginValid() {
-        return isEmailValid() || isPhoneValid();
+    public int find(E element) {
+        for (int i = 0; i < size; i++) {
+            if (element.equals(array[i])) return i;
+        }
+
+        return -1;
     }
 
-    private boolean isPhoneValid() {
-        return Pattern.compile("^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$")
-                .matcher(login)
-                .matches();
-    }
-
-    private boolean isEmailValid() {
-        return Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE)
-                .matcher(login)
-                .matches();
-    }
-}
-
-class Coder {
-    private int code;
-
-    public void sendCode() {
-        code = 111_111 + (int)(Math.random() * ((999_999 - 111_111) + 1));
-
-        System.out.println("Отправлен код " + code);
-    }
-
-    public boolean checkCode(int userCode) {
-        return code == userCode;
+    public String toString() {
+        return Arrays.toString(Arrays.copyOfRange(array, 0, size));
     }
 }
